26c9d87ca5a25c6b3fe0b9ff2b086941ad262c7e
wuzhixu01 <wuzhixu01@baidu.com>
2013-06-18 17:36:55 +0800

    [O] shield the entry of slide unlock screen
    Change-Id: Ibe1fee69d259ae55732a7360ecf08d4baa8960f8

--- custom_app.sh
+++ custom_app.sh
@@ -26,7 +26,7 @@ fi
 if [ $APKNAME == "Settings" ];then
     if [ -f $OUTPATH/res/xml/security_settings_picker.xml ];then
 	echo ">>> begin delete unlock_set_slide in $OUTPATH/res/xml/security_settings_picker.xml"
-        sed -i '/unlock_set_slide/d' $OUTPATH/res/xml/security_settings_picker.xml
+        sed -i '/unlock_set_baidu_slide/d' $OUTPATH/res/xml/security_settings_picker.xml
 	cp $OUTPATH/res/xml/security_settings_picker.xml /tmp/
     fi
 fi
